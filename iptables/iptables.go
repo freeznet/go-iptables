2a21b13941bf6acf146f88c6b1486031c6f71495
Rui Fu <freelz@gmail.com>
2015-09-28 22:21:12 +0800

    remove wait option

--- iptables/iptables.go
+++ iptables/iptables.go
@@ -115,7 +115,7 @@ func (ipt *IPTables) List(table, chain string) ([]string, error) {
 	var stdout, stderr bytes.Buffer
 	cmd := exec.Cmd{
 		Path:   ipt.path,
-		Args:   []string{ipt.path, "--wait", "-t", table, "-S", chain},
+		Args:   []string{ipt.path, "-t", table, "-S", chain},
 		Stdout: &stdout,
 		Stderr: &stderr,
 	}
@@ -160,7 +160,7 @@ func (ipt *IPTables) DeleteChain(table, chain string) error {
 
 func (ipt *IPTables) run(args ...string) error {
 	var stderr bytes.Buffer
-	args = append([]string{"--wait"}, args...)
+	// args = append([]string{"--wait"}, args...)
 	cmd := exec.Cmd{
 		Path:   ipt.path,
 		Args:   append([]string{ipt.path}, args...),
